6fbf986cd09097b69356874898e34fd88a8303c5
Aryan-kanse <138276082+Aryan-kanse@users.noreply.github.com>
2023-07-27 13:45:16 +0530

    Add files via upload

---
+++ py_assignment_5.py
@@ -0,0 +1,16 @@
+t1=[3,6,9,10,25,38,70]
+a=input("enter number: ")
+t1.append(a)
+# print(t1)
+# print("sum:",sum(t1))
+# print("max:",max(t1))
+# print("min:",min(t1))
+# t1.sort()
+# print("sort ascending: ",t1)
+# t1.sort(reverse=True)
+# print("sort descending:",t1)
+# tuple=list(t1)
+# print(tuple)
+ 
+# t1.clear()
+print(t1)
